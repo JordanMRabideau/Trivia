ab9f62250f3bfa311125f617a06c5fa09853b3c2
Jordan Rabideau <jordanmrabideau@gmail.com>
2018-12-19 10:42:38 -0500

    logic finished, just need to do some extra styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -37,10 +37,30 @@ var questionThree = new QMaker(
     "assets/images/roybatty.gif"
 )
 
+var questionFour = new QMaker(
+    "The days of the week have been given names that are derived from the pantheon of which ancient mythology?",
+    "Norse",
+    "Greek",
+    "Babylonian",
+    "Egyptian",
+    "assets/images/thor.gif"
+)
+
+var questionFive = new QMaker(
+    "What is the highest selling videogame console of all time?",
+    "Playstation 2",
+    "Nintendo DS",
+    "Xbox 360",
+    "Gameboy",
+    "assets/images/bike.gif",
+)
+
 var questionArray = [
     questionOne,
     questionTwo,
     questionThree,
+    questionFour,
+    questionFive,
 ];
 // ========================================================================
 
@@ -49,6 +69,8 @@ var timeHolder = 10;
 var correct = 0;
 var incorrect = 0;
 var questionIndex = 0;
+var questionAnswered = false;
+var intervalId;
 
 // This function shuffles the array of choices so that they will be in different button positions on each playthrough
 function choiceShuffler(q) {
@@ -69,7 +91,12 @@ function choiceShuffler(q) {
 }
 
 // Populates the question and the four choices for each question.
-function updateDisplay() {
+function updateQuestion() {
+    questionAnswered = false;
+    console.log(questionIndex);
+    resetTimer();
+    $("#timer").text(timeHolder);
+    console.log(questionArray[questionIndex].question)
     $("#question").text(questionArray[questionIndex].question);
     choiceArray = choiceShuffler(questionArray[questionIndex]);
     $("#choiceOne").text(choiceArray[0][0]).attr("value", choiceArray[0][1]);
@@ -80,26 +107,55 @@ function updateDisplay() {
 
 function endCheck() {
     $(".answerScreen").hide();
-    if (questionIndex === questionArray.length) {
+    if (questionIndex === questionArray.length - 1) {
+        clearInterval(intervalId);
         $(".endScreen").show();
+        $("#correct").text(correct);
+        $("#incorrect").text(incorrect);
     } else {
+        questionIndex++;
+        updateQuestion();
         $(".gameDivs").show()
     }
+   
 }
 
 function resetGame() {
+    console.log(questionIndex)
     questionIndex = 0;
+    console.log(questionIndex)
     correct = 0;
     incorrect = 0;
-    updateDisplay();
+    updateQuestion();
     $(".endScreen").hide();
     $(".gameDivs").show();
 }
+
 function showAnswerScreen() {
     $(".gameDivs").hide();
     $(".answerScreen").show();
-    setTimeout(endCheck, 5000)
+    setTimeout(endCheck, 5000);
 };
+
+function showQuestion() {
+    updateQuestion();
+    $(".instructions").hide();
+    $(".gameDivs").show();
+}
+
+function answerChecker(clicked) {
+    if (clicked.target.attributes.value.nodeValue == 'true') {
+        correct++;
+        $("#check").text("You are correct!");
+        $("#correctAnswer").text("The correct answer was " + questionArray[questionIndex].answer[0]);
+        $("#answerImage").attr("src", questionArray[questionIndex].image)
+    } else {
+        incorrect++;
+        $("#check").text("You are incorrect.");
+        $("#correctAnswer").text("The correct answer was " + questionArray[questionIndex].answer[0]);
+        $("#answerImage").attr("src", questionArray[questionIndex].image)
+    }
+}
 
 function showInstructions() {
     $(".instructions").show();
@@ -108,39 +164,42 @@ function showInstructions() {
     $(".endScreen").hide();
 };
 
-function newGame() {
-    $(".gameDivs").hide();
-    $(".answerScreen").hide();
-    $(".endScreen").hide();
-    $("#playButton").click(function() {
-        $(".instructions").hide();
-        $(".gameDivs").show();
-    })
-
-    // For an example of a timer, go to interval activity in week 5//
-    $(".choice").click(function() {
-        console.log(questionIndex);
-        showAnswerScreen();
-        if(this.value == 'true') {
-            correct++;
-            $("#check").text("You are correct!");
-            $("#correctAnswer").text("The correct answer was " + questionArray[questionIndex].answer[0]);
-            $("#answerImage").attr("src", questionArray[questionIndex].image)
-        } else {
-            incorrect++;
-            $("#check").text("You are incorrect.");
-            $("#correctAnswer").text("The correct answer was " + questionArray[questionIndex].answer[0]);
-            $("#answerImage").attr("src", questionArray[questionIndex].image)
-        }
-        questionIndex++;
-        if (questionIndex < questionArray.length) {
-            updateDisplay();
-        };
-    })
-    updateDisplay();
+function resetTimer() {
+    timeHolder = 10
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
     
 }
 
+function decrement() {
+    --timeHolder;
+    $("#timer").text(timeHolder);
+    if (timeHolder === 0) {
+        incorrect++
+        resetTimer();
+        showAnswerScreen();
+        $("#check").text("Time's Up!");
+        $("#correctAnswer").text("The correct answer was " + questionArray[questionIndex].answer[0]);
+        $("#answerImage").attr("src", questionArray[questionIndex].image)
+    }
+}
+
+
+function newGame() {
+    showInstructions();
+    $("#playButton").click(showQuestion);
+    console.log("correct: " + correct);
+    console.log("incorrect: " + incorrect);
+    $(".choice").click(function(event) {
+        clearInterval(intervalId);
+        answerChecker(event);
+        console.log("correct: " + correct);
+        console.log("incorrect: " + incorrect);
+        showAnswerScreen()
+    });
+    
+    $("#restartButton").click(resetGame);
+};
+
 newGame()
 
-$("#restartButton").click(resetGame);
